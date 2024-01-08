902cdc141db31843760d237abd77a77f92ab946b
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2024-01-08 03:18:04 -0500

    12

--- Milestone-6/api.js
+++ Milestone-6/api.js
@@ -1,10 +1,17 @@
 const loadAllBlissData = ()=>{
     //fetch("https://jsonplaceholder.typicode.com/todos/1")
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    // fetch("https://jsonplaceholder.typicode.com/users")
     //fetch("https://www.ncl.com/cms-service/tap/cruise-ships/BLISS")
     .then(response => response.json())
     .then(data => displayUsers(data))
 }
 function displayUsers(data){
-        console.log(data);
-}
+    const ul = document.getElementById('user-list');
+      for(const user of data){
+        //console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+      }
+    }
