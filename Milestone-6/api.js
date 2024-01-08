a5cdc7825dda6a68a73ccf9d37477d8761039cfb
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2024-01-08 02:55:15 -0500

    qw

--- Milestone-6/api.js
+++ Milestone-6/api.js
@@ -1,6 +1,7 @@
 const loadAllBlissData = ()=>{
     //fetch("https://jsonplaceholder.typicode.com/todos/1")
-    fetch("https://www.ncl.com/cms-service/tap/cruise-ships/BLISS")
+    fetch("https://jsonplaceholder.typicode.com/users")
+    //fetch("https://www.ncl.com/cms-service/tap/cruise-ships/BLISS")
     .then(response => response.json())
     .then(json => console.log(json))
 }
