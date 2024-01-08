af37b181a10f59f227a82147568ee37ae0b120dd
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2024-01-08 03:32:20 -0500

    com

--- Milestone-6/api.js
+++ Milestone-6/api.js
@@ -4,7 +4,8 @@ const loadAllBlissData = ()=>{
     // fetch("https://jsonplaceholder.typicode.com/users")
     //fetch("https://www.ncl.com/cms-service/tap/cruise-ships/BLISS")
     .then(response => response.json())
-    .then(data => displayUsers(data))
+   // .then(data => displayUsers(data))
+    .then(posts => displayPost(posts))
 }
 function displayUsers(data){
     const ul = document.getElementById('user-list');
@@ -14,4 +15,18 @@ function displayUsers(data){
         li.innerText = user.name;
         ul.appendChild(li);
       }
-    }
+    };
+
+function displayPost(posts){
+    const postContainer = document.getElementById('post-list');
+      for(const post of posts){
+        //console.log(user.name);
+        const postDiv = document.createElement('div');
+        postDiv.innerHTML = `
+       <h4>User- ${post.userId} </h4>
+       <h5>Post : ${post.title} </h5>
+       <p>Post ${post.body} </p>
+        `;
+        postContainer.appendChild(postDiv)
+      }
+    }    
